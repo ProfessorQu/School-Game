c6e7201c8dc76cdfb95e3336a781f34afea3d406
ProfessorQu <quint@dasookwat.com>
2023-03-02 21:56:46 +0100

    block

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def main():
                 Vector2(x, y) for x in range(5) for y in range(3)
             ]),
             Room(1, 0, [
-                Vector2(x, y) for x in range(10) for y in range(3, 5)
+                Vector2(x, y) for x in range(10) for y in range(5)
             ])
         ]
     )
