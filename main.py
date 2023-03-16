24aab3329b9bfd5b156ff1dd4752b3997684c244
Gerard12345678 <124671028+Gerard12345678@users.noreply.github.com>
2023-03-16 15:10:45 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from pygame import Vector2
 from src.constants import *
 from src.player import Player
 
+ik ben nog steeds lars
+
 def main():
     # Create a screen, a clock and a player
     SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
