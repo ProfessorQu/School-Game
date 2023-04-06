04ee66ed2220f0058e4acc8221101eb44383441f
ProfessorQu <quint@dasookwat.com>
2023-04-06 20:23:25 +0200

    Muziek weer terug omdat ik dat was vergeten

--- main.py
+++ main.py
@@ -18,13 +18,13 @@ def main():
     CLOCK = pygame.time.Clock()
     player = Player()
 
-    # pygame.mixer.init()
-    # # Load and play the music
-    # pygame.mixer.music.load("assets/sounds/backgroundmusic.ogg")
-    # # Music loopen
-    # pygame.mixer.music.play(-1)
-    # # Volume of the song
-    # pygame.mixer.music.set_volume(0.2)
+    pygame.mixer.init()
+    # Load and play the music
+    pygame.mixer.music.load("assets/sounds/backgroundmusic.ogg")
+    # Music loopen
+    pygame.mixer.music.play(-1)
+    # Volume of the song
+    pygame.mixer.music.set_volume(0.1)
 
     pygame.display.set_caption("School game")
 
